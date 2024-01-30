b7c02db26427fd091bd93e3dcc1b4b8e7dea139e
Nelamin2 <nidalelamin@gmail.com>
2024-01-29 23:02:27 -0800

    task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -4,11 +4,5 @@ if ARGV.empty?
   puts "Usage: #{$PROGRAM_NAME} <string>"
   exit 1
 end
-input_string = ARGV[0]
-regex = /School/
-match_result = input_string.match(regex)
-if match_result
-  puts "#{match_result[0]}$"
-else
-  puts "No match found"
-end
+puts ARGV[0].scan(/School/).join
+
