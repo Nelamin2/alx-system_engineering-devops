746cb656ca0497897fafd3e6dd16b73250cfc202
Nelamin2 <nidalelamin@gmail.com>
2024-04-22 17:25:09 -0700

    tasks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,6 +2,7 @@
 
 """Exports to-do list information for a given employee ID to CSV format."""
 
+import json
 import csv
 import requests
 import sys
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
-        export = {
+    export = {
         user_id: [
             {
                 "task": t.get("title"),
@@ -23,6 +24,5 @@ if __name__ == "__main__":
             for t in todos
         ]
     }
- with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump(export, jsonfile, indent=4)
-
+    with open("{}.json".format(user_id), "w") as jsonfile:
+       json.dump(export, jsonfile, indent=4)
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,11 +7,24 @@ import requests
 import sys
 
 
+user_id = sys.argv[1]
+url = "https://jsonplaceholder.typicode.com/"
+user = requests.get(url + "users/{}".format(user_id)).json()
+username = user.get("username")
+params = {"userId": user_id}
+todos = requests.get(url + "todos", params).json()
+export[user_id] = [
+        {
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": user.get("username"),
+        }
+        for todo in todo_list
+    ]
+
+return export
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    params = {"userId": user_id}
-    todos = requests.get(url + "todos", params).json()
+    data_to_export = fetch_user_data()
+with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(data_to_export, jsonfile, indent=4)
 
