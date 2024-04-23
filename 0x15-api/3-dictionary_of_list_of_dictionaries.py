ff3cd9440be009a8e601332d094fe83ec476079d
Nelamin2 <nidalelamin@gmail.com>
2024-04-22 18:02:20 -0700

    tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,30 +1,31 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 
 """Exports to-do list information for a given employee ID to CSV format."""
 
 import csv
 import requests
 import sys
+import json
 
+def fetch_user_data():
+  url = "https://jsonplaceholder.typicode.com/"
+  users = requests.get(url + "users").json()
+  data_to_export = {}
+  for user in users:
+      user_id = user["id"]
+      user_url = url + f"todos?userId={user_id}"
+      todo_list = requests.get(user_url).json()
 
-user_id = sys.argv[1]
-url = "https://jsonplaceholder.typicode.com/"
-user = requests.get(url + "users/{}".format(user_id)).json()
-username = user.get("username")
-params = {"userId": user_id}
-todos = requests.get(url + "todos", params).json()
-export[user_id] = [
-        {
-            "task": todo.get("title"),
-            "completed": todo.get("completed"),
-            "username": user.get("username"),
-        }
-        for todo in todo_list
-    ]
+      data_to_export[user_id] = [
+          {
+              "task": todo.get("title"),
+              "completed": todo.get("completed"),
+              "username": user.get("username"),
+          }
+          for todo in todo_list
+      ]
 
-return export
 if __name__ == "__main__":
     data_to_export = fetch_user_data()
 with open("todo_all_employees.json", "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
-
