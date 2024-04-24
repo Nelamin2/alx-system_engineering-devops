8f7c51cb33b77d2941a1d8223a2614756897fd2f
Nelamin2 <nidalelamin@gmail.com>
2024-04-24 13:48:53 -0700

    tasks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -25,4 +25,4 @@ if __name__ == "__main__":
         ]
     }
     with open("{}.json".format(user_id), "w") as jsonfile:
-       json.dump(export, jsonfile, indent=4)
+        json.dump(export, jsonfile, indent=4)
