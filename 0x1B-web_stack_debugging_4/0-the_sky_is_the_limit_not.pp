a18489fba61244c7d9321ec203659dc3007907c5
Nelamin2 <nidalelamin@gmail.com>
2024-05-16 10:48:19 -0700

    tasks

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,10 @@
+exec { 'fix --for--nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin', '/bin'],
+}
+
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d',
+}
 exec {'fix --for--nginx':
-command => '/bin/sed -i "s/15/4096" /etc/default/nginx',
-path => '/user/local/bin/:bin',
-}
-exec {'nginex-restart':
-command => '/etc/init.d/nginx restar',
-path => '/etc/init.d',
-}
